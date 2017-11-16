146027bc830184b2e78bac5988674b658ed0c353
Caleb <online@mcdonnell.geek.nz>
2017-11-16 13:46:26 +1300

    Fix PHP warning
    Parameter 2 to wp_hide_post_Public::query_posts_join() expected to be a reference, value given

--- public/class-wp-hide-post-public.php
+++ public/class-wp-hide-post-public.php
@@ -345,7 +345,7 @@ if (!class_exists('wp_hide_post_Public'))
          * @since    1.2.2
          */
 
-        public function query_posts_join($join, &$wp_query)
+        public function query_posts_join($join, $wp_query) // fix warning in query monitor: "Parameter 2 to wp_hide_post_Public::query_posts_join() expected to be a reference, value given"
         {
             if (isset($wp_query->query['wphp_inside_recent_post_sidebar']))
             {
